a90a2b7242e894d793f9384cfc259ad48aeac795
CaioPereir4 <caiocesararagao03@gmail.com>
2024-10-27 18:29:53 -0300

    feat: add a header and chat component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
+import { BrowserModule } from '@angular/platform-browser';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home', component: ChatComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [BrowserModule,RouterModule.forRoot(routes), 
+      
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
